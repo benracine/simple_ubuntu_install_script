4cbfb8ef4c8c71376cebf46f3f5d45ae1bebf2fb
i3enhamin <i3enhamin@ubuntu.(none)>
2011-07-14 15:42:25 -0700

    Init

---
+++ install_script.sh
@@ -0,0 +1,11 @@
+sudo apt-get install vim -y
+sudo apt-get install ipython -y
+sudo apt-get install python-numpy -y
+sudo apt-get install python-scipy -y
+sudo apt-get install python-matplotlib -y
+sudo apt-get install python-setuptools -y
+sudo easy_install pip
+sudo apt-get install git-core -y
+sudo pip install virtualenv
+sudo apt-get install libpq-dev -y
+sudo apt-get install python-dev -y
